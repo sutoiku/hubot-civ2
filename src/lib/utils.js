2536ec241cddc4221fb75b9dbe07ae5ea694a0f4
Yan <yan@sutoiku.com>
2021-04-19 09:33:45 +0200

    Always log

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,13 +3,13 @@
 const IS_TEST = process.env.NODE_ENV === 'test';
 
 exports.log = function log(msg) {
-  if (!IS_TEST) {
-    console.log(msg);
-  }
+  //if (!IS_TEST) {
+  console.log(msg);
+  //}
 };
 
 exports.logError = function logError(msg) {
-  if (!IS_TEST) {
-    console.error(msg);
-  }
+  //if (!IS_TEST) {
+  console.error(msg);
+  //}
 };
