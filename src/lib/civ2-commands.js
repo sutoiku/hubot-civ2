ecabd51eb14545e39d5c82c551672682da9b3e65
Yan <yan@sutoiku.com>
2021-05-05 17:09:22 +0200

    Lint

--- src/lib/civ2-commands.js
+++ src/lib/civ2-commands.js
@@ -5,19 +5,19 @@ const Jenkins = require('jenkins');
 const REQUIRED_STATUS = new Set(['linting', 'unit-tests', 'stoic-assemble', 'stoic-integration-tests']);
 const REVIEW_ICONS = { PENDING: '🟡', COMMENTED: '⚪', APPROVED: '✔️', REQUEST_CHANGES: '❌' };
 
-exports.deployV1 = function(tag) {
+exports.deployV1 = function (tag) {
   return buildJob('Deployment/ci-v1', tag);
 };
 
-exports.deployDocker = function(tag) {
+exports.deployDocker = function (tag) {
   return buildJob('Release/marcus-to-docker-cloud', tag);
 };
 
-exports.deployK8s = function(tag) {
+exports.deployK8s = function (tag) {
   return buildJob('Release/marcus-to-kubernetes', tag);
 };
 
-exports.updateInstance = async function(receivedDomain, env, requestedVersion) {
+exports.updateInstance = async function (receivedDomain, env, requestedVersion) {
   const domain = stripHttp(receivedDomain);
   const version = requestedVersion || (await exports.getLatestVersion());
   const instanceName = 'k8s-' + domain;
@@ -28,29 +28,29 @@ exports.updateInstance = async function(receivedDomain, env, requestedVersion) {
   return version;
 };
 
-exports.release = function(tag, UpdatePivotalAndGitHub) {
+exports.release = function (tag, UpdatePivotalAndGitHub) {
   const additionalParameters = UpdatePivotalAndGitHub ? { UpdatePivotalAndGitHub } : undefined;
   return buildJob('Release/global-release', tag, additionalParameters);
 };
 
-exports.updateBot = function() {
+exports.updateBot = function () {
   return buildJob('Chore/hubot/stoic-hubot/master');
 };
 
-exports.archive = function(repo, branch) {
+exports.archive = function (repo, branch) {
   const { CI_API_ROOT } = getEnv();
   const encodedBranch = encodeURIComponent(branch);
   const url = `${CI_API_ROOT}archive/${repo}/${encodedBranch}`;
   return rp(url, getAuthHeaders());
 };
 
-exports.deleteBranches = async function(branchName, userName) {
+exports.deleteBranches = async function (branchName, userName) {
   const deleted = await ghApi.deleteBranches(branchName, userName);
   const deletedList = deleted.map((it) => '`' + it + '`').join(',');
   return `Delete branches on ${deletedList}.`;
 };
 
-exports.deleteBranch = function(repo, branch) {
+exports.deleteBranch = function (repo, branch) {
   return ghApi.deleteBranch(repo, branch);
 };
 
@@ -62,7 +62,7 @@ exports.destroyFeatureCluster = (FEATURE) => {
   return buildJob('Chore/feature-clusters/destroy', undefined, { FEATURE });
 };
 
-exports.getBranchInformation = async function(branchName, userName) {
+exports.getBranchInformation = async function (branchName, userName) {
   try {
     const status = await ghApi.getAllReposBranchInformation(branchName, userName);
     return formatBranchInformation(branchName, status);
@@ -72,7 +72,7 @@ exports.getBranchInformation = async function(branchName, userName) {
   }
 };
 
-exports.createPRs = async function(branchName, userName, targetBase, options) {
+exports.createPRs = async function (branchName, userName, targetBase, options) {
   try {
     const created = await ghApi.createMissingPrs(branchName, userName, targetBase, options);
 
@@ -94,33 +94,33 @@ exports.createPRs = async function(branchName, userName, targetBase, options) {
   }
 };
 
-exports.mergePRs = async function(branchName, userName) {
+exports.mergePRs = async function (branchName, userName) {
   const merged = await ghApi.mergePRs(branchName, userName);
   const mergedList = merged.map((it) => '`' + it + '`').join(',');
   return `Merging PRs on ${mergedList}.`;
 };
 
-exports.closePRs = async function(branchName, userName) {
+exports.closePRs = async function (branchName, userName) {
   const closed = await ghApi.closePRs(branchName, userName);
   const closedList = closed.map((it) => '`' + it + '`').join(',');
   return `Closed PRs on ${closedList}.`;
 };
 
-exports.announcePRs = async function(brancName, text) {
+exports.announcePRs = async function (brancName, text) {
   const announced = await ghApi.announcePRs(brancName, text);
   const announcedList = announced.map((it) => '`' + it + '`').join(',');
   return `Announced on ${announcedList}.`;
 };
 
-exports.updatePRsDescriptions = async function(branchName, userName) {
+exports.updatePRsDescriptions = async function (branchName, userName) {
   return ghApi.updatePRsDescriptions(branchName, userName);
 };
 
-exports.commentPtReferences = async function(branchName) {
+exports.commentPtReferences = async function (branchName) {
   return ghApi.commentPtReferences(branchName);
 };
 
-exports.getLatestVersion = async function(instance = 'latest') {
+exports.getLatestVersion = async function (instance = 'latest') {
   const suffix = instance === 'dev' ? 'com' : 'cc';
   const url = `https://${instance}.stoic.${suffix}/health`;
   const healthBody = await rp(url);
@@ -128,7 +128,7 @@ exports.getLatestVersion = async function(instance = 'latest') {
   return version;
 };
 
-exports.replicatedPromotion = async function(channel, version) {
+exports.replicatedPromotion = async function (channel, version) {
   return buildJob('Deployment/promote-replicated', undefined, { channel, version, releaseNotes: '' });
 };
 
@@ -210,7 +210,7 @@ function getRepoReport(repoName, branchName, data) {
 
   return {
     mergeable: statusReport.mergeable && approved,
-    message: ` * <${repoUrl}|${repoName}> : ${prStatus} - ${statusMessage} - ${reviewsMessage}`
+    message: ` * <${repoUrl}|${repoName}> : ${prStatus} - ${statusMessage} - ${reviewsMessage}`,
   };
 }
 
@@ -262,7 +262,7 @@ function getStatusReport({ status }) {
 
   return {
     message: okStatus === totalStatus ? ':white_check_mark:' : `${okStatus}/${totalStatus} are OK ${nonOkMessage}`,
-    mergeable: okStatus === totalStatus
+    mergeable: okStatus === totalStatus,
   };
 }
 
