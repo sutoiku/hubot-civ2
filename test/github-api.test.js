4939ab802fcf3df36e538565af11c4f5c89bfb54
Pierre Lebouc <pierre@sutoiku.com>
2022-05-10 14:47:27 +0200

    Fix tests

--- test/github-api.test.js
+++ test/github-api.test.js
@@ -5,7 +5,7 @@ describe('Github API helpers', () => {
   describe('getReposAndIssuesId', () => {
     const scenarios = [
       {
-        input: 'bug/test!components.stoic-33',
+        input: 'bug/test__components.stoic-33',
         expectation: { repoName: 'components.stoic', issueNumber: '33' },
       },
       {
@@ -13,11 +13,11 @@ describe('Github API helpers', () => {
         expectation: { repoName: undefined, issueNumber: undefined },
       },
       {
-        input: 'branch/test!kyu',
+        input: 'branch/test__kyu',
         expectation: { repoName: undefined, issueNumber: undefined },
       },
       {
-        input: 'fix/my-branch!praxis-42!particula-12',
+        input: 'fix/my-branch__praxis-42__particula-12',
         expectation: { repoName: 'praxis', issueNumber: '42' },
       },
     ];
@@ -32,11 +32,11 @@ describe('Github API helpers', () => {
   describe('getPrTextWithGitHubIssue', () => {
     const scenarios = [
       {
-        input: 'bug/test!components.stoic-33',
+        input: 'bug/test__components.stoic-33',
         expectation: {
           description: '# Github\n\n - https://github.com/sutoiku/components.stoic/issues/33',
-          id: 'bug/test!components.stoic-33-components.stoic-33',
-          name: 'bug/test!components.stoic-33',
+          id: 'bug/test__components.stoic-33-components.stoic-33',
+          name: 'bug/test__components.stoic-33',
         },
       },
       {
@@ -48,19 +48,19 @@ describe('Github API helpers', () => {
         },
       },
       {
-        input: 'branch/test!kyu',
+        input: 'branch/test__kyu',
         expectation: {
           description: '# Github\n\n Github issue not found',
-          id: 'branch/test!kyu',
-          name: 'branch/test!kyu',
+          id: 'branch/test__kyu',
+          name: 'branch/test__kyu',
         },
       },
       {
-        input: 'fix/my-branch!praxis-42!particula-12',
+        input: 'fix/my-branch__praxis-42__particula-12',
         expectation: {
           description: '# Github\n\n - https://github.com/sutoiku/praxis/issues/42',
-          id: 'fix/my-branch!praxis-42!particula-12-praxis-42',
-          name: 'fix/my-branch!praxis-42!particula-12',
+          id: 'fix/my-branch__praxis-42__particula-12-praxis-42',
+          name: 'fix/my-branch__praxis-42__particula-12',
         },
       },
     ];
